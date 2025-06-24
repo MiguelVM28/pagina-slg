cb49b513a3e4b4b7f8f7150cb35b5c4e3dc4c1c5
MiguelVM28 <MiguelVM28@gmail.com>
2025-06-23 23:26:30 -0500

    Se agrega Proyecto

---
+++ home.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colegio San Luis Gonzaga</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+
+    <header class="header">
+        <div class="top-bar">
+            <div class="container top-bar-container">
+                <div class="contact-item">
+                    <i class="fas fa-phone"></i> <span>964359449</span>
+                </div>
+                <div class="contact-item">
+                    <i class="fas fa-envelope"></i> <span>admision@ieslg.edu.pe</span>
+                </div>
+                <div class="contact-item">
+                    <i class="fas fa-globe"></i> <span>SIEWEB</span>
+                </div>
+                <div class="contact-item">
+                    <i class="fas fa-envelope"></i> <span>admision@csf.edu.pe</span>
+                </div>
+            </div>
+</div>
+
+    </div>
+        <nav class="navbar">
+            <div class="container">
+                <div class="logo-centered">
+                    <img src="img.png" alt="Colegio Nacional "San Luis Gonzaga">
+                    <span>- 279 años -</span>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="#">Sobre Nosotros</a></li>
+                    <li class="dropdown">
+                        <a href="#">Niveles <i class="fas fa-caret-down"></i></a>
+                        <div class="dropdown-content">
+                            <a href="#">Nivel Secundaria</a>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Admisión <i class="fas fa-caret-down"></i></a>
+                        <div class="dropdown-content">
+                            <a href="#">Proceso de Admisión</a>
+                            <a href="#">Proceso de Matrícula</a>
+                            <a href="#">Lista de Útiles</a>
+                        </div>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Area <i class="fas fa-caret-down"></i></a>
+                        <div class="dropdown-content">
+                            <a href="#">Matemática</a>
+                            <a href="#">Comunicación</a>
+                            <a href="#">Ingles</a>
+                            <a href="#">Ciencias</a>
+                            <a href="#">Arte</a>
+                        </div>
+                    </li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Galería</a></li>
+                    <li><a href="#">Trabaja con nosotros</a></li>
+                    <li class="social-icons">
+                        <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+
+                <div class="hero-image carousel">
+                    <img src="escoltaieslg.png" alt="Imagen 1" class="active">
+                    <img src="bandaslg.png" alt="Imagen 2">
+                    <img src="atras.png" alt="Imagen 3">
+                </div>
+    </section>
+
+    <section class="why-choose-us">
+        <div class="container">
+            <h2>¿Por qué elegir al Colegio San Luis Gonzaga?</h2>
+            <div class="reasons-grid">
+                <div class="reason-item">
+                    <img src="medalla.png" alt="279 years">
+                    <p>279 años al servicio de la educación</p>
+                </div>
+                <div class="reason-item">
+                    <img src="pergamino.png" alt="Humanist model icon">
+                    <p>Modelo de enseñanza humanista</p>
+                </div>
+                <div class="reason-item">
+                    <img src="birrete.png" alt="Social responsibility icon">
+                    <p>Cultura de equipo y responsabilidad social</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="complementary-services">
+        <div class="container">
+            <div class="services-content">
+                <h2>SERVICIOS COMPLEMENTARIOS</h2>
+                <div class="service-item">
+                    <img src="basquet.png" alt="Football icon">
+                    <h3>Talleres de Fútbol, Básquet y Vóley</h3>
+                    <p>Los estudiantes podrán dominar mejor el deporte, disfrutar del juego en equipo, participar en torneos y aumentar su capacidad física.</p>
+                </div>
+                <div class="service-item">
+                    <img src="reforzamiento.png" alt="Reinforcement classes icon">
+                    <h3>Clases de Reforzamiento</h3>
+                    <p>Son esenciales para potenciar la metodología de estudio, trabajar la memoria y comprender de manera efectiva la solución a distintos problemas.</p>
+                </div>
+                <div class="service-item">
+                    <img src="debate.png" alt="Debate workshops icon">
+                    <h3>Talleres de Debate</h3>
+                    <p>El niño aprenderá a rebatir y defender su postura frente a otros participantes. Descubrirá que a través de argumentos válidos puede convencer o influir en los demás.</p>
+                </div>
+                <button class="admission-process-button">CONOCE EL PROCESO DE ADMISIÓN</button>
+            </div>
+            <div class="services-image">
+                <img src="brasilslg.png" alt="Playground">
+            </div>
+        </div>
+    </section>
+
+<section class="areas-psychopedagogy">
+    <div class="container">
+
+        <section class="especialidades-section">
+            <div class="titulo-especialidades">
+                <h2>ESPECIALIDADES</h2>
+                <p>Conoce todas las especialidades que ofrecemos para formar a nuestros estudiantes.</p>
+        </div>
+
+        <div class="area-cards">
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-administracion.png" alt="ADM Y COM">
+                        <h3>ADMINISTRACION Y COMERCIO</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>ADMINISTRACION Y COMERCIO</h3>
+                        <p>Espacio para fomentar la creatividad a través del arte.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-albañil-construccion-sanitaria.png" alt="ALBA Y CONS SANI">
+                        <h3>ALBAÑIL, CONSTRUCCION SANITARIA</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>ALBAÑIL, CONSTRUCCION SANITARIA</h3>
+                        <p>Fomentamos el trabajo en equipo y la actividad física saludable.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-automotores.png" alt="TALLER AUTOMOTORES">
+                        <h3>TALLER AUTOMOTORES</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER AUTOMOTORES</h3>
+                        <p>Preparamos a los alumnos con un enfoque comunicativo en inglés.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front"> 
+                        <img src="taller-carpinteria.png" alt="TALLER CARPINTERIA">
+                        <h3>TALLER CARPINTERIA</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER CARPINTERIA</h3>
+                        <p>Fortalece el crecimiento y desarrollo de los alumnos.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-cuero-calzado.png" alt="TALLER CUERO Y CALZADO">
+                        <h3>TALLER CUERO Y CALZADO</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER CUERO Y CALZADO</h3>
+                        <p>Desarrollamos pensamiento crítico y habilidades científicas desde temprana edad.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-electricidad.png" alt="TALLER ELECTRICIDAD">
+                        <h3>TALLER ELECTRICIDAD</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER ELECTRICIDAD</h3>
+                        <p>Fomentamos el razonamiento lógico y la resolución de problemas.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-electronica.png" alt="TALLER ELECTRONICA">
+                        <h3>TALLER ELECTRONICA</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER ELECTRONICA</h3>
+                        <p>Potenciamos habilidades de lectura, escritura y expresión oral.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-informatico.png" alt="TALLER INFORMATICO">
+                        <h3>TALLER INFORMATICO</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER INFORMATICO</h3>
+                        <p>Reflexionamos sobre el entorno, la cultura y la convivencia democrática.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-matriceria.png" alt="TALLER MATRICERIA">
+                        <h3>TALLER MATRICERIA</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER MATRICERIA</h3>
+                        <p>Promovemos valores, respeto y reflexión espiritual en los estudiantes.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-mecanica-produccion.png" alt="TALLER MECANICA PRODUCCION">
+                        <h3>TALLER MECANICA PRODUCCION</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER MECANICA PRODUCCION</h3>
+                        <p>Fomentamos hábitos saludables y disciplina mediante el deporte.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card" onclick="this.classList.toggle('rotada')">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="taller-mecanica.png" alt="TALLER MECANICA">
+                        <h3>TALLER MECANICA</h3>
+                    </div>
+                    <div class="flip-card-back">
+                        <h3>TALLER MECANICA</h3>
+                        <p>Introducimos a los estudiantes en el uso responsable de la tecnología.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+    <section class="student-preferences">
+        <div class="container">
+            <h2>Preferencias de nuestros alumnos</h2>
+            <div class="university-logos">
+                <img src="logo-san-juan.png" alt="SJB Logo">
+                <img src="logo-san-luis.png" alt="SLG Logo">
+                <img src="UTP-Logo.png" alt="UTP Logo">
+                <img src="CONTINENTAL.png" alt="Universidad Continental">
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-left">
+                <img src="img.png" alt="San Luis G Logo Footer">
+                <p>Ofrecemos un modelo de enseñanza humanista que prioriza la formación integral de la persona y el desarrollo de su capacidad crítica y de libre expresión, en un clima de calidez y confianza.</p>
+            </div>
+            <div class="footer-middle">
+                <h3>Enlaces de Interés</h3>
+                <ul>
+                    <li><a href="#">Sobre Nosotros</a></li>
+                    <li><a href="#">Niveles</a></li>
+                    <li><a href="#">Admisión</a></li>
+                    <li><a href="#">Área</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Galería</a></li>
+                    <li><a href="#">Trabaja con nosotros</a></li>
+                </ul>
+            </div>
+            <div class="footer-right">
+                <h3>Contáctenos</h3>
+                <p><i class="fas fa-phone"></i> (511) 964359449</p>
+                <p><i class="fas fa-envelope"></i> admision@csf.edu.pe</p>
+                <p><i class="fas fa-map-marker-alt"></i> Av. Gregorio Escobedo 895, Jesús María. Lima - Perú</p>
+                <div class="social-media-footer">
+                    <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+        <a href="https://wa.me/yourphonenumber" class="whatsapp-float" target="_blank">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+    </footer>
+
+    <script>
+        let current = 0;
+        const images = document.querySelectorAll('.carousel img');
+
+        setInterval(() => {
+            images[current].classList.remove('active');
+            current = (current + 1) % images.length;
+            images[current].classList.add('active');
+        }, 3000); // Cambia cada 3 segundos
+        </script>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
